358786e3cd8da28a1bb62b7a0fbdf000a41a7e35
Learn with fun <80329368+Dearam@users.noreply.github.com>
2021-08-14 20:19:46 +0530

    Create App.js

---
+++ static/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.scss'
+import './App.css'
+import Nav from './components/Nav';
+import Tform from './components/Tform';
+import Todo from './components/Todo';
+import Dchart from './components/Dchart';
+
+function App() {
+  return (
+    <div className="app">
+    <Nav/>
+    </div>
+    
+  )
+}
+
+export default App;
